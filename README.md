43c5124cd5d9c3fafde879148de748304237e1d5
ThomasR <thomasr@fantasymail.de>
2019-09-12 01:29:08 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Maven Web Fragment project example for IntelliJ IDEA.
+
+Requires an installation of [Tomcat EE Plus](http://archive.apache.org/dist/tomee/tomee-7.0.5/apache-tomee-7.0.5-plus.zip).
+
+Discussion see [https://stackoverflow.com/q/31340562/5134084](https://stackoverflow.com/q/31340562/5134084)
---
+++ src/main/java/com/example/MyServlet.java
@@ -0,0 +1,35 @@
+package com.example;
+
+import org.apache.catalina.servlets.DefaultServlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.Writer;
+
+public class MyServlet extends DefaultServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+        String resourcePath = request.getPathInfo();
+        if (resourcePath.contains("/static")) {
+            HttpServletRequestWrapper wrappedRequest = new HttpServletRequestWrapper(request) {
+                @Override
+                public String getPathInfo() {
+                    return "/static.html";
+                }
+            };
+            super.serveResource(wrappedRequest, response, true, fileEncoding);
+            return;
+        }
+
+        response.setContentType("text/html");
+
+        Writer writer = response.getWriter();
+        writer.write("<p>Hello World!</p>");
+        writer.write("<a href=\"static\">Static resource</a>");
+    }
+}
